2d0e4f2c5cb7d4b9ae80127ac1c88001b07158c7
umeshpalsingh <umeshpal6789@gmail.com>
2022-04-30 18:16:51 +0530

    Instruction card changes

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -1,44 +1,34 @@
 import React from 'react';
-import { Card, Typography, Chip, Box } from "@mui/material";
+import { Card, Typography, Chip } from "@mui/material";
 import SunLogo from "../images/Logo.svg";
 
 function Instruction() {
     return (
-        <Card elevation={0} className="instruction-card"
+        <Card elevation={4} className="instruction-card"
             sx={{
-                backgroundColor: '#111827',
+                backgroundColor: '#0D1117',
                 borderRadius: '20px'
             }}>
             <div>
-                <img alt='Umesh Pal Singh' src={SunLogo}
-                    style={{ width: "200px", height: "200px", borderRadius: "100px", margin: '20px auto', filter: 'drop-shadow(0 0 5px #eee)' }} />
+                <img alt='Umesh Pal Singh' src={SunLogo} className="Logo" />
             </div>
-            <Typography align='center'
-                sx={{
-                    fontFamily: 'Poppins, monospace',
-                    color: '#eee',
-                    fontSize: '1.2rem',
-                    mb: '20px'
-                }}>
-                Find Weather
+            <Chip
+                label="Find Weather"
+                variant='outlined'
+                sx={{ color: 'common.white', mt: '5px', fontFamily: 'Poppins' }}
+                component='a'
+                href='https://umeshpalsingh.github.io/findweather/'
+                clickable />
+            <Typography align='center' component='p' variant='p'
+                sx={{ color: '#eee', fontFamily: 'Poppins, Lato', fontSize: '.85rem', padding: '10px', maxWidth: '95%', margin: '20px auto' }}>
+                Click on the input and enter the city/state/country name to find the weather information. Please do not make any typing mistakes for better results.
             </Typography>
-            <Box sx={{
-                margin: '20px auto 10px',
-                padding: '20px 0'
-            }}>
-                <Typography align='left'
-                    sx={{ color: '#eee', fontFamily: 'Poppins, Lato', fontSize: '.85rem', padding: '10px' }}>
-                    Instruction : click on the input and enter the city/state/country name to find the weather information.<br />Note : please do not make any typing mistakes for better results.
-                </Typography>
-            </Box>
-            <Box>
-                <Chip align='center' label="Developed with ❤️ by Umesh Pal Singh"
-                    sx={{
-                        fontSize: '.8rem',
-                        color: '#eee',
-                        fontFamily: 'Poppins'
-                    }} />
-            </Box>
+            <Typography
+                variant='body1'
+                component='p'
+                sx={{ color: 'common.white', fontSize: '.7rem', fontFamily: 'Poppins', margin: '10px auto' }}>
+                Developed by Umesh Pal Singh
+            </Typography>
         </Card>
     );
 };
